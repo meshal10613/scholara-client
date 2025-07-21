a5258646647109500d82c9ba7b3c567424d0c4d4
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-07-21 17:17:56 +0600

    some apply changes

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -48,7 +48,7 @@ const DashboardLayout = () => {
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 dashlink">
+                <ul className="menu bg-base-200 text-base-content min-h-full w-64 2xl:w-80 p-4 dashlink">
                     <Logo/>
                     {/* Sidebar content here */}
                     {
