a48e6c12020a4de6a7e4abe08618a9e7e1f68362
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-08-16 22:56:11 +0600

    banner link added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // import default styles
+import { Link } from 'react-router';
 
 const Banner = () => {
     const slides = [
         {
         id: 1,
+        link: "/all-scholarship",
         image: "https://i.ibb.co/MDBBG9N6/2148950574.jpg",
         heading: "Unlock Your Future",
         subtext: "Apply now for scholarships and pursue your dreams.",
@@ -13,6 +15,7 @@ const Banner = () => {
         },
         {
         id: 2,
+        link: "/all-scholarship",
         image: "https://images.unsplash.com/photo-1573164574572-cb89e39749b4",
         heading: "Empowering Education",
         subtext: "Scholarships for every level — from school to university.",
@@ -20,6 +23,7 @@ const Banner = () => {
         },
         {
         id: 3,
+        link: "/dashboard/my-profile",
         image: "https://images.unsplash.com/photo-1571260899304-425eee4c7efc",
         heading: "Your Journey Starts Here",
         subtext: "Join a community of scholars and achievers.",
@@ -44,7 +48,7 @@ const Banner = () => {
                 <div className="absolute inset-0 bg-black/50 flex flex-col justify-center items-start px-10 text-white">
                 <h2 className="text-3xl md:text-5xl font-bold">{slide.heading}</h2>
                 <p className="mt-4 text-lg md:text-xl">{slide.subtext}</p>
-                <button className="btn btn-secondary text-base-100 mt-6">{slide.buttonText}</button>
+                <Link to={`${slide.link}`} className="btn btn-secondary text-base-100 mt-6">{slide.buttonText}</Link>
                 </div>
             </div>
             ))}
